f738b0ea429c050446c6e25f7c9a6b1549328d59
Kieran Ahn <kieranahn@gmail.com>
2022-11-11 11:36:02 -0800

    Removed redundant comments

--- src/services/articleService.js
+++ src/services/articleService.js
@@ -7,10 +7,6 @@
  * Adapted from Dr. Toal's notes.
  */
 
-// This service completely hides the data store from the rest of the app.
-// No other part of the app knows how the data is stored. If anyone wants
-// to read or write data, they have to go through this service.
-
 import { db } from '../firebaseConfig'
 import { collection, query, getDocs, addDoc, deleteDoc, orderBy, limit, Timestamp } from 'firebase/firestore'
 
