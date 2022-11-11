1139349a0ad681a504e83ad39cc3859b572cf3dc
Kieran Ahn <kieranahn@gmail.com>
2022-11-11 11:36:28 -0800

    Updated css

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,23 @@
 export default function Nav({ articles, setArticle, pageNum, setPageNum }) {
-  console.log(articles.slice(10 * pageNum, 10 * (pageNum + 1)))
+  console.log(loading)
   return (
     <nav>
-      {!articles || (pageNum + 1) * 10 > Math.ceil(articles.length / 10) * 10
-        ? 'No articles\n'
-        : articles.slice(10 * pageNum, 10 * (pageNum + 1)).map(a => (
-            <p key={a.id} onClick={() => setArticle(a)}>
-              {a.title}
-            </p>
-          ))}
-      <button onClick={() => setPageNum(pageNum - 1)} className="pageNav" disabled={pageNum === 0}>
-        Previous Page
-      </button>
-      <p className="pageNav">{pageNum + 1}</p>
-      <button onClick={() => setPageNum(pageNum + 1)} className="pageNav">
-        Next Page
-      </button>
+      {!articles || (pageNum + 1) * 10 > Math.ceil(articles.length / 10) * 10 ? (
+        <span>No Articles</span>
+      ) : (
+        articles.slice(10 * pageNum, 10 * (pageNum + 1)).map(a => (
+          <p key={a.id} onClick={() => setArticle(a)}>
+            {a.title}
+          </p>
+        ))
+      )}
+      <footer className="pageNav">
+        <button onClick={() => setPageNum(pageNum - 1)} disabled={pageNum === 0}>
+          Previous Page
+        </button>
+        <span>{pageNum + 1}</span>
+        <button onClick={() => setPageNum(pageNum + 1)}>Next Page</button>
+      </footer>
     </nav>
   )
 }
